b3ad9bca055358e9a2e2f118e9f8705fc6e79845
aqsa707 <146213719+aqsa707@users.noreply.github.com>
2024-02-09 10:22:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// Canvas Setup
+var cnv = document.getElementById("myCanvas");
+var ctx = cnv.getContext("2d");
+cnv.width = 650;
+cnv.height = 650;
+
+// Background Sky
+ctx.fillStyle = "lightblue";
+ctx.fillRect(0, 0, 650, 650);
+
+// sun
+
+// mountain 1
+
+// snow on mountain 1
+
+// mountain 2
+
+// snow on mountain 2
+
+// mountain 3
+
+// snow on montain 3
+
+// tree 1 + trunk
+
+// tree 2 + trunk
+
+// cloud img(s)
